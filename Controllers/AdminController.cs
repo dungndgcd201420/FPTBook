1917d9c216ab8354f2096312dcc0085b827a1065
hnkhanh266 <kobalt.hnkhanh@gmail.com>
2022-08-13 20:08:43 +0700

    Add Customer and Store Owner accounts list to Admin Controller - Add Back to Index button

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPTBook.Data;
+using FPTBook.Models;
+using FPTBook.Utils;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FPTBook.Controllers
 {
+    [Authorize(Roles = Role.ADMIN)]
     public class AdminController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(
+          UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Customers()
+        {
+            var usersWithPermission = _userManager.GetUsersInRoleAsync(Role.CUSTOMER).Result;
+            return View(usersWithPermission);
+        }
+
+        [HttpGet]
+        public IActionResult StoreOwners()
+        {
+            var usersWithPermission = _userManager.GetUsersInRoleAsync(Role.OWNER).Result;
+            return View(usersWithPermission);
+        }
+
     }
 }
